5465899d63ea1b16741a87eb6467c2131c37e950
Joey Cho <cjy12182000@naver.com>
2018-07-22 16:41:52 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,8 @@ VALUES ('Mr Chocolate');
 INSERT INTO bears (name)
 VALUES ('Rowdy');
 INSERT INTO bears (name)
+VALUES ('Tabitha');
+INSERT INTO bears (name)
 VALUES ('Sergeant Brown');
 INSERT INTO bears (name)
 VALUES ('Melissa');
